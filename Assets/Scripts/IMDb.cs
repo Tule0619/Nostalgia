a5694afaa2e1147d24f985b3abc6d3f533b5a424
Joshua Ward (RIT Student) <jlw5291@rit.edu>
2024-10-05 17:01:52 -0400

    IMDb script

---
+++ Assets/Scripts/IMDb.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class IMDb
+{
+    string title;
+    int iteration;
+    
+    string[] suffixes =
+    {
+        "In Space!",
+        "Bigger and Better",
+        "The Final Hour",
+        "Endgame",
+        "World War",
+        "Nevermore",
+        "Cash Grab",
+        "Curse of the Thingamabob",
+        "Unleashed",
+        "The Revenge",
+        "Apocalypse",
+        "Mission Improbable",
+        "Oops, All Sequels!",
+        "Now in 3D!",
+        "Definitive Edition",
+        "The Snyder Cut",
+        "Tokyo Drift",
+        "The Quantum Dimension",
+        "The Christmas Special",
+        "Back In Time",
+        "The Finale (Part 1)",
+        "CGI Mayhem",
+        "Back To Basics",
+        "Halloween Night",
+        "Calm Before The Storm",
+        "Seafarers",
+        "The Tax Write-Off"
+    };
+
+    public IMDb(string title)
+    {
+        this.title = title;
+        iteration = 0;
+    }
+    
+    public string NewTitle()
+    {
+        switch (++iteration) {
+            case 1: return title;
+            case 2: return title + " " + iteration;
+            default: return title + " " + iteration + (Random.Range(0, 2) == 0 ? ": " + suffixes[Random.Range(0, suffixes.Length)] : "");
+        }
+    }
+}
