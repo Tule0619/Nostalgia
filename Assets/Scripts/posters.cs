079240a8d637417dd14cb37973ff820ed98f1297
BlueAur <jc7467@g.rit.edu>
2024-10-05 23:39:38 -0400

    posters implemented

--- Assets/Scripts/posters.cs
+++ Assets/Scripts/posters.cs
@@ -1,19 +1,19 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class posters : MonoBehaviour
 {
-    [SerializeField] Texture2D[] textures; 
+    [SerializeField] Sprite[] textures;
+    [SerializeField] UnityEngine.UI.Image poster;
     // Start is called before the first frame update
-    void Start()
+    public void changePoster()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        poster.sprite = textures[Random.Range(0, textures.Length)];
+        Debug.Log("HI!!!! IM RUNNING");
     }
 }
