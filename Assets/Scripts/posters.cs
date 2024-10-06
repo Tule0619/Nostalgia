fd41b260fd15920a818b5f2e2297e5b3ee4c5b7a
Jeff Chen (RIT Student) <jc7117@rit.edu>
2024-10-06 10:29:21 -0400

    built

--- Assets/Scripts/posters.cs
+++ Assets/Scripts/posters.cs
@@ -1,10 +1,4 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class posters : MonoBehaviour
 {
